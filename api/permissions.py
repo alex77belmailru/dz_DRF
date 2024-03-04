d95e93760e210420e560fbe98af48df7fee17206
alex77bel <alex77bel@yandex.ru>
2024-03-04 13:21:36 +0300

    4/3/2024

--- api/permissions.py
+++ api/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class AdminOrReadOnly(BasePermission):
     """Проверка, что пользователь Админ, или вызван безопасный метод"""
 
-    def has_permission(self, request, view):
+    def has_permission(self, request, view):  # работает на уровне всего запроса
         if request.user.is_superuser:  # модератор имеет доступ ко всему
             return True
 
@@ -14,5 +14,5 @@ class AdminOrReadOnly(BasePermission):
 class IsOwner(BasePermission):
     """Проверка, что пользователь - владелец"""
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):  # работает на уровне отдельно объекта бд
         return request.user == obj
